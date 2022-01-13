ffe24f258e636c890100d586747304177d6ca4b0
__Umarbek__ <u902004668@gmail.com>
2022-01-13 13:25:03 +0500

    message

---
+++ ToasterLibrary/src/main/java/uz/umarbek/toasterlibrary/ToasterMessage.kt
@@ -0,0 +1,13 @@
+package uz.umarbek.toasterlibrary
+
+import android.content.Context
+import android.widget.Toast
+
+
+ object ToasterMessage {
+
+    fun s(c: Context?, message: String?) {
+
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
+    }
+}
